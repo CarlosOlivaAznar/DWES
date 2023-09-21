c25691d39f1f0ddde9636e00aa46ab3908c8bdbc
CarlosOlivaAznar <carlosoliva5503@email.com>
2023-09-21 17:45:31 +0200

    comentario ejercicio 6

--- evaluacion_inicial/ejercicio6.php
+++ evaluacion_inicial/ejercicio6.php
@@ -7,25 +7,10 @@
 </head>
 <body>
     <?php
-        $num = rand(0,500);
-
-        echo "$num <br>";
-
-        if($num >= 400){
-            echo "El numero aleatorio se encuentra en el rango [400,500]";
-        } else if($num >= 300){
-            echo "El numero aleatorio se encuentra en el rango [300,400]";
-        } else if($num >= 200){
-            echo "El numero aleatorio se encuentra en el rango [200,300]";
-        } else if($num >= 100){
-            echo "El numero aleatorio se encuentra en el rango [100,200]";
-        } else if($num >= 0){
-            echo "El numero aleatorio se encuentra en el rango [0,100]";
-        }
-
+        //Ejercicio sin comenzar
     ?>
     <br>
     <br>
-    <a href="ejercicio5.html">volver al cuestionario</a>
+    <a href="ejercicio6.html">volver al cuestionario</a>
 </body>
 </html>
